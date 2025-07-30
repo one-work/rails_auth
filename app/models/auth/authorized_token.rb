f69a7ac16646b7f40e0fded7f4a9d1bee9bec8d7
qinmingyuan <mingyuan0715@foxmail.com>
2025-07-30 11:18:27 +0800

    rag

--- app/models/auth/authorized_token.rb
+++
@@ -1,7 +0,0 @@
-module Auth
-  class AuthorizedToken < ApplicationRecord
-    include Model::AuthorizedToken
-    include Org::Ext::AuthorizedToken if defined? RailsOrg
-    include Wechat::Ext::AuthorizedToken if defined? RailsWechat
-  end
-end
