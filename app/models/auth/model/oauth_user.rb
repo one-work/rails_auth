81059ea48e7977d1e18e5ae7985a3ffac52c1b11
qinmingyuan <mingyuan0715@foxmail.com>
2025-08-01 22:24:28 +0800

    user id

--- app/models/auth/model/oauth_user.rb
+++ app/models/auth/model/oauth_user.rb
@@ -29,7 +29,7 @@ module Auth
 
       belongs_to :account, -> { where(confirmed: true) }, foreign_key: :identity, primary_key: :identity, inverse_of: :oauth_users, optional: true
 
-      has_many :sessions, primary_key: [:uid, :appid, :identity], foreign_key: [:uid, :appid, :identity], dependent: :delete_all
+      has_many :sessions, primary_key: [:uid, :appid, :identity, :user_id], foreign_key: [:uid, :appid, :identity, :user_id], dependent: :delete_all
       belongs_to :same_oauth_user, ->(o) { where.not(id: o.id) }, class_name: self.name, foreign_key: :unionid, primary_key: :unionid, optional: true
       has_many :same_oauth_users, class_name: self.name, primary_key: :unionid, foreign_key: :unionid
 
