f552e3ca5373832f5ab804a3555c08be5abc30b9
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-03 16:23:36 +0800

    combine oauth user and account

--- app/models/auth/model/account.rb
+++ app/models/auth/model/account.rb
@@ -3,88 +3,20 @@ module Auth
     extend ActiveSupport::Concern
 
     included do
-      attribute :identity, :string, index: true
-      attribute :confirmed, :boolean
-      attribute :source, :string
-      index [:identity, :confirmed]
 
-      belongs_to :user, optional: true
 
-      has_many :sessions, primary_key: [:identity, :user_id], foreign_key: [:identity, :user_id], dependent: :delete_all
-      has_many :verify_tokens, primary_key: :identity, foreign_key: :identity, dependent: :delete_all
-      has_many :oauth_users, primary_key: :identity, foreign_key: :identity, inverse_of: :account
 
-      scope :without_user, -> { where(user_id: nil) }
-      scope :with_user, -> { where.not(user_id: nil) }
-      scope :confirmed, -> { where(confirmed: true) }
 
-      validates :identity, presence: true, uniqueness: { scope: [:confirmed, :source] }
 
-      normalizes :identity, with: -> (email) { email.strip.downcase }
+
+
 
 
       # belongs_to 的 autosave 是在 before_save 中定义的
       #
-      after_validation :init_user, if: -> { confirmed? && confirmed_changed? }
     end
 
-    def last?
-      user.accounts.where.not(id: self.id).empty?
-    end
 
-    def init_user
-      user || build_user
-    end
-
-    def can_login_by_token?(params)
-      user || build_user
-      user.assign_attributes params.slice(
-        'name',
-        'password',
-        'password_confirmation',
-        'invited_code'
-      ) # 这里必须用 String 类型，因为params 转义过来的hash key 是字符
-      user.last_login_at = Time.current
-      self.confirmed = true
-
-      self.class.transaction do
-        user.save!
-        self.save!
-      end
-
-      user
-    end
-
-    def can_login_by_password?(password)
-      user.can_login?(password)
-    end
-
-    def xx
-      if params[:device_id]
-        account = DeviceAccount.find_by identity: params[:device_id]
-        self.user = account.user if account
-      end
-    end
-
-    def authorized_token
-      authorized_tokens.find(&:effective?) || authorized_tokens.create
-    end
-
-    def auth_token
-      authorized_token.id
-    end
-
-    def once_token
-      auth_token
-    end
-
-    def verify_token
-      verify_tokens.find(&:effective?) || verify_tokens.create
-    end
-
-    def reset_notice
-      p 'Should implement in subclass'
-    end
 
   end
 end
--- app/models/auth/model/oauth_user.rb
+++ app/models/auth/model/oauth_user.rb
@@ -21,22 +21,37 @@ module Auth
       attribute :identity, :string, index: true
       attribute :online_at, :datetime
       attribute :offline_at, :datetime
+      attribute :confirmed, :boolean
+      attribute :source, :string
 
       index [:uid, :type], unique: true
+      index [:identity, :confirmed]
 
       belongs_to :user, optional: true
       belongs_to :organ, class_name: 'Org::Organ', optional: true
 
-      has_one :account, -> { where(confirmed: true) }, foreign_key: :identity, primary_key: :identity, inverse_of: :oauth_users
       has_many :sessions, primary_key: [:uid, :appid, :identity, :user_id], foreign_key: [:uid, :appid, :identity, :user_id], dependent: :delete_all
+      has_many :verify_tokens, primary_key: :identity, foreign_key: :identity, dependent: :delete_all
 
       belongs_to :same_oauth_user, ->(o) { where.not(id: o.id) }, class_name: self.name, foreign_key: :unionid, primary_key: :unionid, optional: true
       has_many :same_oauth_users, class_name: self.name, primary_key: :unionid, foreign_key: :unionid
 
+      scope :without_user, -> { where(user_id: nil) }
+      scope :with_user, -> { where.not(user_id: nil) }
+      scope :confirmed, -> { where(confirmed: true) }
+
+      normalizes :identity, with: -> (email) { email.strip.downcase }
+
+
       validates :uid, presence: true
+      validates :identity, uniqueness: { scope: [:confirmed, :source] }
+
+
 
       before_save :auto_link, if: -> { unionid.present? && unionid_changed? }
       before_save :init_account, if: -> { identity_changed? }
+      after_validation :init_user, if: -> { confirmed? && confirmed_changed? }
+
       after_save :sync_to_authorized_tokens, if: -> { saved_change_to_identity? }
       after_save :sync_name_to_user, if: -> { name.present? && saved_change_to_name? }
       after_save_commit :sync_avatar_to_user_later, if: -> { avatar_url.present? && saved_change_to_avatar_url? }
@@ -118,6 +133,52 @@ module Auth
 
     def auth_jwt_token
       authorized_token.generate_jwt_token
+    end
+
+    def last?
+      user.accounts.where.not(id: self.id).empty?
+    end
+
+    def can_login_by_token?(params)
+      user || build_user
+      user.assign_attributes params.slice(
+        'name',
+        'password',
+        'password_confirmation',
+        'invited_code'
+      ) # 这里必须用 String 类型，因为params 转义过来的hash key 是字符
+      user.last_login_at = Time.current
+      self.confirmed = true
+
+      self.class.transaction do
+        user.save!
+        self.save!
+      end
+
+      user
+    end
+
+    def can_login_by_password?(password)
+      user.can_login?(password)
+    end
+
+    def xx
+      if params[:device_id]
+        account = DeviceAccount.find_by identity: params[:device_id]
+        self.user = account.user if account
+      end
+    end
+
+    def authorized_token
+      authorized_tokens.find(&:effective?) || authorized_tokens.create
+    end
+
+    def once_token
+      auth_token
+    end
+
+    def verify_token
+      verify_tokens.find(&:effective?) || verify_tokens.create
     end
 
     def refresh_token!
