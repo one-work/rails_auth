38acdd7c0c7c0db9b3b54fd77c5666f41c5035dd
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-22 22:33:55 +0800

    model.id

--- app/controllers/auth/apps_controller.rb
+++ app/controllers/auth/apps_controller.rb
@@ -5,7 +5,8 @@ module Auth
     def index
       @app_views = AppView.includes(app: { logo_attachment: :blob }).where(session_id: session.id.to_s).order(view_at: :desc).limit(4)
       @all_apps = AppView.includes(app: { logo_attachment: :blob }).where(session_id: session.id.to_s).page(params[:page]).limit(4)
-      @apps = App.with_attached_logo.page(params[:page])
+      @star_ids = AppView.where(session_id: session.id.to_s, starred: true).pluck(:id)
+      @apps = App.includes(:starred_app).with_attached_logo.page(params[:page])
     end
 
     def star
